710ab633974338a364eea02ca9424d8233478020
NSMohamedElAlfy <mo7amedelalfy@gmail.com>
2017-03-13 06:09:58 +0200

    Initial Commit

---
+++ nsinstagram/nsinstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nsinstagram
+//
+//  Created by NSMohamedElalfy on 3/13/17.
+//  Copyright © 2017 NSMohamedElalfy. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    
+  }
+}
+
